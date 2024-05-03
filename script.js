5cde8ff557380263298b0276ec33fb2cd106f7b0
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-05-03 18:23:48 +0530

    first

---
+++ script.js
@@ -0,0 +1,51 @@
+function loading() {
+  var tl = gsap.timeline();
+
+  tl.to("#yellow1", {
+    top: "-100%",
+    delay: 0.4,
+    duration: 0.7,
+    ease: "expo.out",
+  });
+
+  tl.from(
+    "#yellow2",
+    {
+      top: "100%",
+      delay: 0.7,
+      duration: 0.7,
+      ease: "expo.out",
+    },
+    "anim"
+  );
+  tl.to(
+    "#loader h1",
+    {
+      delay: 0.2,
+      duration: 0.7,
+      color: "black",
+    },
+    "anim"
+  );
+
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+
+loading();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+var elems = document.querySelectorAll(".elem");
+var page2 = document.querySelector("#page2");
+
+elems.forEach(function (ele) {
+  ele.addEventListener("mouseenter", function () {
+    var bgimg = ele.getAttribute("data-img");
+    page2.style.backgroundImage = `url(${bgimg})`;
+  });
+});
