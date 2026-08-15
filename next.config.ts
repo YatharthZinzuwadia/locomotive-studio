bad83a4188e2be78243b94109eca0bf04c0adc25
yatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2026-08-16 03:58:02 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/locomotive-studio",
-  assetPrefix: "/locomotive-studio/",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
