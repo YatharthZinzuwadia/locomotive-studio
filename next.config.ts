32740cf532cb8023c54ae7c1d80d3f4d2a957d84
yatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2026-08-16 04:00:21 +0530

    yml

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/locomotive-studio",
+  assetPrefix: "/locomotive-studio/",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
